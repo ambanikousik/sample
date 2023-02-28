eeb5cc96f4e6ae8dd7f301f23493a6d4ef59a9bd
Kousik Ambani <rikousik@gmail.com>
2023-02-28 14:37:50 +0600

    sample_tree_view

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="main">
+    <h1>Demo View Tree</h1>
+    <vue-tree :node="root" />
+  </div>
+</template>
+
+<script>
+import VueTree from "./components/vueTree.vue";
+
+export default {
+  components: {
+    VueTree,
+  },
+  data() {
+    return {
+      root: {
+        name: "Main Folder",
+        children: [
+          { name: "music" },
+          { name: "workspace" },
+          { name: "New Folder" },
+          { name: "Video" },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  /* padding: 20px; */
+  font-size: x-large;
+  font-weight: bold;
+  /* text-align: center; */
+}
+
+h1 {
+  background-color: aqua;
+}
+</style>
---
+++ src/components/vueTree.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="tree">
+    <div @click="nodeClicked" class="root">
+      {{ node.name }}
+      <span v-if="hasChildren" class="icon">{{
+        expended ? "&minus;" : "&plus;"
+      }}</span>
+    </div>
+
+    <div class="child" v-if="expended">
+      <vueTree v-for="child in node.children" :key="child.name" :node="child" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "vueTree",
+  props: {
+    node: Object,
+  },
+  data() {
+    return {
+      expended: false,
+    };
+  },
+  methods: {
+    nodeClicked() {
+      this.expended = !this.expended;
+    },
+  },
+  computed: {
+    hasChildren() {
+      return this.node.children;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.root {
+  margin-top: 15px;
+
+}
+
+.child {
+  padding-left: 5px;
+  margin-left: 20px;
+  border-left: 2px solid #ccc;
+}
+
+.icon {
+  margin-left: 130px;
+
+
+}
+</style>
